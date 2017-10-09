48c711dd0fb7cb21756fe92b56968e6c9ec27a10
Rishabh Sairawat <rishabh.sairawat01@gmail.com>
2017-10-10 02:50:16 +0530

    Create CountingSort.java

---
+++ Algorithms/Sorting/CountingSort.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int[] arr=new int[N];
+        for (int i = 0; i < N; i++) {
+             arr[i]=s.nextInt();
+        }
+        int[] sortedArr=new int[arr.length];
+		countSort(arr,sortedArr);
+    }
+    private static void countSort(int[] arr,int[] sortedArr) {
+		int max=0;
+		for(int i=0;i<arr.length;i++){
+			max=Math.max(max, arr[i]);
+		}
+		int aux[]=new int[max+1];
+		for(int i=0;i<aux.length;i++){
+			aux[i]=0;
+		}
+		for(int i=0;i<arr.length;i++){
+			aux[arr[i]]++;
+		}
+		for(int i=0;i<aux.length;i++){
+			if(aux[i]>0){
+			    System.out.println(i+" "+aux[i]);
+			}
+		}
+
+	}
+}
